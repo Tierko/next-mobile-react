c91c572a8b90d7aeb35ddc33a8c556607aebdb1f
Ilya Altukhov <zompin@artlebedev.ru>
2018-06-18 19:15:56 +0300

    Readme

---
+++ deploy.js
@@ -0,0 +1,29 @@
+const FtpDeploy = require('ftp-deploy');
+const credentials = require('./ftp-config.json');
+const ftpDeploy = new FtpDeploy();
+
+const config = {
+  user: credentials.user,
+  password: credentials.password,
+  host: 'six',
+  port: 21,
+  localRoot: __dirname,
+  remoteRoot: '/next-mobile/www/',
+  include: ['*'],
+  exclude: [
+    'node_modules/**/*',
+    '.git/**/*',
+    '.idea/**/*',
+    'src/**/*',
+    'README.md',
+    'accesses.txt',
+    'deploy.js',
+    'package-lock.json',
+    'package.json',
+    'webpack.config.js',
+  ],
+};
+
+ftpDeploy.deploy(config)
+  .then(() => console.log('finished'))
+  .catch(e => console.log(e));
