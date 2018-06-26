47def2c757f02dd2871af7a19b6f52fdf167893c
Ilya Altukhov <zompin@artlebedev.ru>
2018-06-26 14:21:07 +0300

    Overview fix

--- src/js/components/Remain.jsx
+++ src/js/components/Remain.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ProgressLinear from './ProgressLinear';
 import Button from './Button';
 
@@ -11,7 +12,7 @@ const Remain = ({ data, buy }) => (
         <div key={i.id} className="remain__item">
           <div className="remain__desc">
             <div><span>{(i.current + '').replace('.', ',')} {i.unit}</span> из {i.max}</div>
-            <div className="remain__service">{i.name}</div>
+            <Link className="remain__service" to="#">{i.name}</Link>
           </div>
           <ProgressLinear color="red" max={i.max} current={i.current} />
         </div>
