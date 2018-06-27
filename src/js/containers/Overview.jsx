94db35263f3a8630d33aa87f6ac4be38aa9444e7
Ilya Altukhov <zompin@artlebedev.ru>
2018-06-27 12:06:05 +0300

    Overview fix

--- src/js/containers/Overview.jsx
+++ src/js/containers/Overview.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MobileNav from '../components/MobileNav';
 import Aside from '../components/Aside';
 import Footer from '../components/Footer';
@@ -131,5 +132,9 @@ class Overview extends Component {
     ];
   }
 }
+
+Overview.propTypes = {
+  history: PropTypes.shape().isRequired,
+};
 
 export default Overview;
