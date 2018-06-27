41be73243bc903b2c5b63c7e49837981b70fe331
Ilya Altukhov <zompin@artlebedev.ru>
2018-06-27 13:10:42 +0300

    Conditions fix

--- src/js/containers/Conditions.jsx
+++ src/js/containers/Conditions.jsx
@@ -9,14 +9,12 @@ const Conditions = () => (
     <NavLobby back={`${Pages.SignUp}/step/1`} />
     <div className="conditions__content">
       <h1>Условия перехода</h1>
+      <div>Номер оформлен на ваше имя</div>
+      <div>Нет задолженности перед оператором</div>
+      <div>Вы не меняли оператора в последние 2 месяца</div>
       <div className="conditions__desc">Если хоть один пункт нарушен – обратитесь <Link to={Pages.Support} className="link">в поддержку</Link>, и мы подскажем, что делать</div>
-      <div>
-        <div>Номер оформлен на ваше имя</div>
-        <div>Нет задолженности перед оператором</div>
-        <div>Вы не меняли оператора в последние 2 месяца</div>
-        <Button className="button_conditions" onClick={()=>{}}>Далее</Button>
-        <div className="conditions__note">К заполнению заявки</div>
-      </div>
+      <Button className="button_conditions" onClick={()=>{}}>Далее</Button>
+      <div className="conditions__note">К заполнению заявки</div>
     </div>
   </div>
 );
