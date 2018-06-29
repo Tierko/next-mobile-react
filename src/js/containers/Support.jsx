002e14068bbc0f6515d890b1be372c59938225c0
Ilya Altukhov <zompin@artlebedev.ru>
2018-06-29 13:30:53 +0300

    Support fix

--- src/js/containers/Support.jsx
+++ src/js/containers/Support.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import MobileNav from '../components/MobileNav';
+import NavLobby from '../components/NavLobby';
 import Chat from '../components/Chat';
 
 class Support extends Component {
   render() {
-    return (
+    return [
+      <MobileNav type="enter" />,
+      <NavLobby />,
       <div className="support">
-        <div className="support__aside"></div>
+        <div className="support__aside" />
         <div className="support__chat">
           <Chat />
         </div>
-      </div>
-    );
+      </div>,
+    ];
   }
 }
 
