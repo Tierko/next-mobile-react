bfca575fb4c5e464fdcdbf4ef38dd8ee76d048ef
Ilya Altukhov <zompin@artlebedev.ru>
2018-07-06 13:14:36 +0300

    History fix

--- src/js/components/Expense.jsx
+++ src/js/components/Expense.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Grade from './Grade';
 import ProgressLinear from './ProgressLinear';
@@ -28,28 +28,37 @@ class Expense extends Component {
     const { data } = this.props;
     const { itemId } = this.state;
     const item = itemId === -1 ? data[data.length - 1] : data.find(d => d.id === itemId);
-    const cost = item.expense.reduce((acc, d) => (acc + d.cost), 0);
+    const cost = item && item.expense.reduce((acc, d) => (acc + d.cost), 0);
 
     return (
       <div className="expense">
-        <Grade data={data} onItemSelect={onMonthSelect} wide />
-        <div className="expense__total">
-          <div className="expense__total-date">{months[item.month]} {item.year}</div>
-          <div className="expense__total-cost">{formatCost(cost)}</div>
-        </div>
+        {
+          !item &&
+          <div className="expense__empty">У вас пока нет расходов</div>
+        }
+        {
+          !!item &&
+          <Fragment>
+            <Grade data={data} onItemSelect={onMonthSelect} wide />
+            <div className="expense__total">
+              <div className="expense__total-date">{months[item.month]} {item.year}</div>
+              <div className="expense__total-cost">{formatCost(cost)}</div>
+            </div>
 
-        <div className="expense__details">
-          {
-            item.expense.map(e => (
-              <div key={e.id} className="expense__detail">
-                <div className="expense__detail-title">{e.title}</div>
-                <ProgressLinear className="progress-linear_expense" current={e.cost} max={cost} tall />
-                <div className="expense__detail-cost">{formatCost(e.cost)}</div>
-              </div>
-            ))
-          }
-        </div>
-        <Button className="button_expense" onClick={orderDetails}>Заказать детализацию</Button>
+            <div className="expense__details">
+              {
+                item.expense.map(e => (
+                  <div key={e.id} className="expense__detail">
+                    <div className="expense__detail-title">{e.title}</div>
+                    <ProgressLinear className="progress-linear_expense" current={e.cost} max={cost} tall />
+                    <div className="expense__detail-cost">{formatCost(e.cost)}</div>
+                  </div>
+                ))
+              }
+            </div>
+            <Button className="button_expense" onClick={orderDetails}>Заказать детализацию</Button>
+          </Fragment>
+        }
       </div>
     );
   }
