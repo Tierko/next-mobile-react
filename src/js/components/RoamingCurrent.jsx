6e0bc993f65ea0903cfbf744dd4c92f857d66a5e
Ilya Altukhov <zompin@artlebedev.ru>
2018-07-18 12:00:41 +0300

    RoamingCurrent

---
+++ src/js/components/RoamingCurrent.jsx
@@ -0,0 +1,76 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import ProgressLinear from './ProgressLinear';
+import Button from './Button';
+import CheckboxSlide from './CheckboxSlide';
+import { formatCost } from '../utils';
+import { Pages } from '../constants';
+
+class RoamingCurrent extends Component {
+  state = {
+    slowInternet: false,
+  };
+
+  onChange = (name, value) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  addPackage = () => {
+    const { history, data: { id } } = this.props;
+
+    history.push(`${Pages.ROAMING}/internet/${id}`);
+  };
+
+  render() {
+    const { addPackage, onChange } = this;
+    const { data } = this.props;
+    const { slowInternet } = this.state;
+
+    return (
+      <Fragment>
+        <div className="roaming-current__subtitle roaming-current__subtitle_fast">
+          Пакет быстрого интернета <span className="roaming-current__note_desktop">(еще 30 дней)</span>
+        </div>
+        <div>
+          <span className="roaming-current__big">12,01 ГБ</span> <span className="roaming-current__note">из 20</span> <span className="roaming-current__note roaming-current__note_mobile">(еще 30 дней)</span>
+        </div>
+        <ProgressLinear className="progress-linear_roaming" max={20} current={12.01} />
+        <Button className="button_roaming-add" onClick={addPackage}>
+          Добавить пакет
+        </Button>
+        <div className="roaming-current__subtitle">
+          Помегабайтный интернет <span className="roaming-current__note">(заработает, когда закончится пакет)</span>
+        </div>
+        <div className="roaming-current__regular">
+          <div>
+            <span className="roaming-current__big">{formatCost(data.tariff.internet.byMb)}</span> <span className="roaming-current__note">/ Мб</span>
+          </div>
+          <CheckboxSlide className="checkbox-slide_roaming" value={slowInternet} name="slowInternet" onChange={onChange} />
+        </div>
+        <div className="roaming-current__tariff">
+          <div className="roaming-current__tariff-item">
+            <div>Звонки</div>
+            <div>
+              <span className="roaming-current__big">от {data.tariff.calls} ₽</span> <span>/ мин</span>
+            </div>
+          </div>
+          <div className="roaming-current__tariff-item">
+            <div>Сообщения</div>
+            <div>
+              <span className="roaming-current__big">от 7 ₽</span> <span>/ СМС</span>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+RoamingCurrent.propTypes = {
+  history: PropTypes.shape().isRequired,
+  data: PropTypes.shape().isRequired,
+};
+
+export default RoamingCurrent;
--- src/js/components/RoamingZone.jsx
+++ src/js/components/RoamingZone.jsx
@@ -2,41 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cs from 'classnames';
 import { Link } from 'react-router-dom';
-import ProgressLinear from './ProgressLinear';
-import Button from './Button';
-import CheckboxSlide from './CheckboxSlide';
+import RoamingCurrent from './RoamingCurrent';
 import { Pages, COUNTRIES } from '../constants';
-import { formatCost, convertStrings } from '../utils';
+import { convertStrings } from '../utils';
 
 class RoamingZone extends Component {
-  state = {
-    slowInternet: false,
-  };
-
-  onChange = (name, value) => {
-    this.setState({
-      [name]: value,
-    });
-  };
-
   getCountries = () => {
-    // const filtered = [];
-    const { data, features, zones } = this.props;
+    const { data, features } = this.props;
     let countries = features.filter(f => data.countries.indexOf(f.properties.iso_a2) !== -1);
-
-    // if (countries.length === 0) {
-    //   zones.forEach((z) => {
-    //     if (z.id === data.id) {
-    //       return false;
-    //     }
-    //
-    //     z.countries.forEach(c => filtered.push(c));
-    //   });
-    // }
-    //
-    // if (filtered.length) {
-    //   countries = features.filter(f => data.countries.indexOf(f.properties.iso_a2) === -1);
-    // }
 
     countries = countries.map(c => c.properties.name);
 
@@ -48,17 +21,9 @@ class RoamingZone extends Component {
     return countries.join(', ');
   };
 
-  addPackage = () => {
-    const { history, data: { id } } = this.props;
-
-    history.push(`${Pages.ROAMING}/internet/${id}`);
-  };
-
   render() {
-    const { slowInternet } = this.state;
-    const { onChange, addPackage, getCountries } = this;
-    const { active, data } = this.props;
-
+    const { getCountries } = this;
+    const { active, data, history } = this.props;
 
     return (
       <div className={cs('roaming-zone', { 'roaming-zone_show': data.id === active })}>
@@ -66,39 +31,7 @@ class RoamingZone extends Component {
         <Link to={`${Pages.ROAMING}/countries/${data.id}`} className="roaming-zone__countries">
           {getCountries()}
         </Link>
-        <div className="roaming-zone__subtitle roaming-zone__subtitle_fast">
-          Пакет быстрого интернета <span className="roaming-zone__note_desktop">(еще 30 дней)</span>
-        </div>
-        <div>
-          <span className="roaming-zone__big">12,01 ГБ</span> <span className="roaming-zone__note">из 20</span> <span className="roaming-zone__note roaming-zone__note_mobile">(еще 30 дней)</span>
-        </div>
-        <ProgressLinear className="progress-linear_roaming" max={20} current={12.01} />
-        <Button className="button_roaming-add" onClick={addPackage}>
-          Добавить пакет
-        </Button>
-        <div className="roaming-zone__subtitle">
-          Помегабайтный интернет <span className="roaming-zone__note">(заработает, когда закончится пакет)</span>
-        </div>
-        <div className="roaming-zone__regular">
-          <div>
-            <span className="roaming-zone__big">{formatCost(data.tariff.internet.byMb)}</span> <span className="roaming-zone__note">/ Мб</span>
-          </div>
-          <CheckboxSlide className="checkbox-slide_roaming" value={slowInternet} name="slowInternet" onChange={onChange} />
-        </div>
-        <div className="roaming-zone__tariff">
-          <div className="roaming-zone__tariff-item">
-            <div>Звонки</div>
-            <div>
-              <span className="roaming-zone__big">от {data.tariff.calls} ₽</span> <span>/ мин</span>
-            </div>
-          </div>
-          <div className="roaming-zone__tariff-item">
-            <div>Сообщения</div>
-            <div>
-              <span className="roaming-zone__big">от 7 ₽</span> <span>/ СМС</span>
-            </div>
-          </div>
-        </div>
+        <RoamingCurrent data={data} history={history} />
         <Link to={`${Pages.ROAMING}/zone-tariff/${data.id}`} className="roaming-zone__more">
           Подробнее о тарифах в {data.title}
         </Link>
