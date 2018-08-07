975690d89f00296483e3fac6fb2e59e8acf9b32d
Ilya Altukhov <zompin@artlebedev.ru>
2018-08-07 11:20:56 +0300

    Footer fix

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -6,11 +6,11 @@ const Footer = () => (
       &copy; 2018 Next Mobile
     </div>
     <div className="footer__dev">
-      <a href="https://www.artlebedev.ru/">
+      <a className="footer__dev-link" href="https://www.artlebedev.ru/">
         <img className="footer__dev-img" src="/media/images/als-logo.svg" alt="Студия Артеми Лебедева" />
       </a>
       <div className="footer__dev-text">
-        Задизайнено в <a href="https://www.artlebedev.ru/" className="footer__link">Студии Артемия&nbsp;Лебедева</a>
+        Задизайнено в <a href="https://www.artlebedev.ru/" className="footer__link footer__link_stud">Студии Артемия&nbsp;Лебедева</a>
         <br />
         <a href="https://www.artlebedev.ru/" className="footer__link">Информация о сайте</a>
       </div>
