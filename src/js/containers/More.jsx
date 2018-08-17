cd273ae7a0716125cd04d3790b80eb4217f9a6b8
Ilya Altukhov <zompin@artlebedev.ru>
2018-08-17 17:48:19 +0300

    More fix

--- src/js/containers/More.jsx
+++ src/js/containers/More.jsx
@@ -18,35 +18,37 @@ class More extends Component {
         <Transitions>
           <div className="dashboard__content">
             <LinkBack className="link-back_offset-bottom" href={Pages.OVERVIEW} />
-            {
-              type === 'calls' &&
-              <Fragment>
-                <div className="dashboard__header">Звонки по тарифу Супервип</div>
-                <div className="more__text">На номера Next всей России дома и в поездках по стране</div>
-                <div className="more__subtitle">0 ₽ / мин</div>
-                <div className="more__text">На номера всех операторов Москвы и на Next по всей России </div>
-                <div className="more__subtitle">550 мин / месяц </div>
-                <InterCalls more className="inter-calls_more" />
-                <div className="dashboard__header dashboard__header_more">Дополнительный пакет</div>
-                <div className="more__subtitle">До 12 сентбря</div>
-                <div className="more__text">На номера всех операторов Москвы и на Next по всей России </div>
-                <div className="more__subtitle">550 мин / месяц </div>
-                <div className="more__text">На номера Next всей России дома и в поездках по стране</div>
-                <div className="more__subtitle">0 ₽ / мин</div>
-              </Fragment>
-            }
-            {
-              type === 'internet' &&
-              <Fragment>
-                <div className="dashboard__header">Интернет по тарифу Супервип</div>
-                <div className="more__text">Объем трафика</div>
-                <div className="more__subtitle">18 Гб / месяц</div>
-                <div className="dashboard__header dashboard__header_more">Дополнительный пакет</div>
-                <div className="more__subtitle">До 12 сентбря</div>
-                <div className="more__text">Объем купленного трафика</div>
-                <div className="more__subtitle">2 Гб</div>
-              </Fragment>
-            }
+            <div>
+              {
+                type === 'calls' &&
+                <Fragment>
+                  <div className="dashboard__header dashboard__header_more">Звонки по тарифу Супервип</div>
+                  <div className="more__text">На номера Next всей России дома и в поездках по стране</div>
+                  <div className="more__subtitle">0 ₽ / мин</div>
+                  <div className="more__text">На номера всех операторов Москвы и на Next по всей России </div>
+                  <div className="more__subtitle">550 мин / месяц </div>
+                  <InterCalls more className="inter-calls_more" />
+                  <div className="dashboard__header dashboard__header_more">Дополнительный пакет</div>
+                  <div className="more__subtitle">До 12 сентбря</div>
+                  <div className="more__text">На номера всех операторов Москвы и на Next по всей России </div>
+                  <div className="more__subtitle">550 мин / месяц </div>
+                  <div className="more__text">На номера Next всей России дома и в поездках по стране</div>
+                  <div className="more__subtitle">0 ₽ / мин</div>
+                </Fragment>
+              }
+              {
+                type === 'internet' &&
+                <Fragment>
+                  <div className="dashboard__header dashboard__header_more">Интернет по тарифу Супервип</div>
+                  <div className="more__text">Объем трафика</div>
+                  <div className="more__subtitle">18 Гб / месяц</div>
+                  <div className="dashboard__header dashboard__header_more">Дополнительный пакет</div>
+                  <div className="more__subtitle">До 12 сентбря</div>
+                  <div className="more__text">Объем купленного трафика</div>
+                  <div className="more__subtitle">2 Гб</div>
+                </Fragment>
+              }
+            </div>
           </div>
         </Transitions>
       </div>,
