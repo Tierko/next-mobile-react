518289710061d899e1271e25a8f59c77b96463f7
Ilya Altukhov <zompin@artlebedev.ru>
2018-08-17 16:18:15 +0300

    MobileNav fix

--- src/js/components/MobileNav.jsx
+++ src/js/components/MobileNav.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import cs from 'classnames';
 import MobileNavEnter from '../components/MobileNavEnter';
@@ -41,19 +41,22 @@ class MobileNav extends Component {
     const { toggle } = this;
 
     return (
-      <div className="mobile-nav" ref={(e) => { this.nav = e; }}>
-        <div onClick={toggle} className={cs('mobile-nav__button', { 'mobile-nav__button_open': open })} />
-        <div className={cs('mobile-nav__list', { 'mobile-nav__list_open': open })}>
-          {
-            type === 'enter' &&
-            <MobileNavEnter />
-          }
-          {
-            type === 'dashboard' &&
-            <MobileNavDashboard />
-          }
+      <Fragment>
+        <div className={cs('mobile-nav__drawer', { 'mobile-nav__drawer_show': open })} />
+        <div className="mobile-nav" ref={(e) => { this.nav = e; }}>
+          <div onClick={toggle} className={cs('mobile-nav__button', { 'mobile-nav__button_open': open })} />
+          <div className={cs('mobile-nav__list', { 'mobile-nav__list_open': open })}>
+            {
+              type === 'enter' &&
+              <MobileNavEnter />
+            }
+            {
+              type === 'dashboard' &&
+              <MobileNavDashboard />
+            }
+          </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
