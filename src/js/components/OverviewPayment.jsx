f0cfa27dad6497930870c83331d720bd735458e0
Ilya Altukhov <zompin@artlebedev.ru>
2018-08-31 11:09:41 +0300

    Overview fix

--- src/js/components/OverviewPayment.jsx
+++ src/js/components/OverviewPayment.jsx
@@ -9,12 +9,13 @@ import { Pages } from '../constants';
 const OverviewPayment = ({ sum, onChange, onPay }) => (
   <Fragment>
     <div className="overview-payment">
-      <Limit sum={sum} className="" />
       <div className="overview-payment__inner">
         <Link to={Pages.PAY} className="overview-payment__title">Пополнение:</Link>
         <Input value={sum} onChange={onChange} name="sum" className="input_payment" />
+        <Limit sum={sum} className="limit_overview-mobile" />
         <Button className="button_payment" onClick={onPay} disabled={sum > 15000 || sum < 100}>Оплатить</Button>
       </div>
+      <Limit sum={sum} className="limit_overview-desktop" />
     </div>
   </Fragment>
 );
