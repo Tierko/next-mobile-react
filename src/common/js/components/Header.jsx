4d999ec8c017d1fa10eebae6ac3315f7a9990e15
Ilya Altukhov <zompin@artlebedev.ru>
2018-09-03 18:25:09 +0300

    Header fix

--- src/common/js/components/Header.jsx
+++ src/common/js/components/Header.jsx
@@ -5,19 +5,27 @@ import { Pages } from '../../../cabinet/js/constants';
 
 const Header = ({ back }) => (
   <nav className="header">
-    {
-      back &&
-      <Link to={back} className="header__back">
-        <img src="/media/icons/back.svg" alt="" />
-      </Link>
-    }
-    <NavLink className="header__item" to={Pages.SIGN_IN} exact>Вход</NavLink>
-    <div className="header__span" />
-    <NavLink className="header__item" to={Pages.SIGN_UP}>Регистрация</NavLink>
-    <div className="header__span" />
-    <NavLink className="header__item" to={Pages.SUPPORT}>Поддержка</NavLink>
-    <div className="header__span" />
-    <NavLink className="header__item" to={Pages.REQUEST_STATUS}>Статус заявки</NavLink>
+    <div className="header__left">
+      <div className="header__logo" />
+      {
+        back &&
+        <Link to={back} className="header__back">
+          <img src="/media/icons/back.svg" alt="" />
+        </Link>
+      }
+    </div>
+    <div className="header__center">
+      <NavLink className="header__item" to={Pages.REQUEST_STATUS}>Тарифы и услуги</NavLink>
+      <div className="header__span" />
+      <NavLink className="header__item" to={Pages.REQUEST_STATUS}>Статус заявки</NavLink>
+      <div className="header__span" />
+      <NavLink className="header__item" to={Pages.SUPPORT}>Поддержка</NavLink>
+    </div>
+    <div className="header__right">
+      <NavLink className="header__item" to={Pages.SIGN_IN} exact>Вход</NavLink>
+      <div className="header__span" />
+      <NavLink className="header__item" to={Pages.SIGN_UP}>Регистрация</NavLink>
+    </div>
   </nav>
 );
 
