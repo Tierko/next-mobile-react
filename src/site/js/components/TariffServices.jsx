436508a712a846d8390d915c6f7f65ab788642f6
Ilya Altukhov <zompin@artlebedev.ru>
2018-09-06 18:54:10 +0300

    Tariff draft

---
+++ src/site/js/components/TariffServices.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TariffServices = () => (
+  <div className="tariff-services">
+    <div className="tariff-services__header">Услуги</div>
+    <div className="tariff-services__row">
+      <div className="tariff-services__title">Кто звонил</div>
+      <div className="tariff-services__text">Мы&nbsp;будем присылать СМС о&nbsp;том, кто вам звонил, пока вы&nbsp;были недоступны.</div>
+      <div className="tariff-services__text">Бесплатно</div>
+    </div>
+    <div className="tariff-services__row">
+      <div className="tariff-services__title">Антиопределитель номера</div>
+      <div className="tariff-services__text">Ваш номер нельзя будет определить при звонке.</div>
+      <div className="tariff-services__text">Бесплатно</div>
+    </div>
+  </div>
+);
+
+export default TariffServices;
--- src/site/js/containers/Trariff.jsx
+++ src/site/js/containers/Trariff.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from '../../../common/js/components/Header';
+import TariffServices from '../components/TariffServices';
 
 const Tariff = () => (
-  <div>Tariff</div>
+  <div className="tariff">
+    <Header />
+    <div className="tariff__inner">
+      <div>Тарифы</div>
+      <TariffServices />
+    </div>
+  </div>
 );
 
 export default Tariff;
