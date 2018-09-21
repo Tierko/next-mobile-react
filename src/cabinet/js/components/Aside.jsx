c20f9f16437d2395820f886f5df41df1c80ece3e
Ilya Altukhov <zompin@artlebedev.ru>
2018-09-21 15:09:07 +0300

    Aside fix

--- src/cabinet/js/components/Aside.jsx
+++ src/cabinet/js/components/Aside.jsx
@@ -1,69 +1,142 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, Link } from 'react-router-dom';
+import cs from 'classnames';
 import { formatCost, getData } from '../utils';
 import { Pages } from '../constants';
 
-const getIsActive = (a, b) => {
-  console.log(a, b)
-}
+// const getIsActive = (exact, modifier) => {
+//   const links = document.querySelectorAll('.aside__link');
+//
+//   if (links.length) {
+//     Array.prototype.forEach.call(links, (l) => {
+//       l.classList.remove('aside__link_not-exact');
+//     });
+//   }
+//
+//   if (exact && !exact.isExact) {
+//     const link = document.querySelector(`.aside__link_${modifier}`);
+//
+//     if (link) {
+//       link.classList.add('aside__link_not-exact');
+//     }
+//
+//     console.log(link)
+//   }
+//
+//   return 'active---';
+// };
 
-const Aside = ({ hideLink, hideNav }) => {
-  const traffic = getData('remain')[0].current.toString().replace('.', ',');
-  const calls = getData('remain')[1].current;
+class Aside extends Component {
+  state = {
+    current: '',
+  };
 
-  return (
-    <div className="aside">
-      <div className="aside__logo">
-        <img src="/media/images/logo-blue.png" alt="Next Mobile" />
+  setActiveLink = (match, modifier) => {
+    const { current } = this.state;
+
+    if (modifier === 'exit') {
+      return false;
+    }
+
+    if (match && !match.isExact && current !== modifier) {
+      this.setState({
+        current: modifier,
+      });
+    }
+
+    if (match && match.isExact && current) {
+      this.setState({
+        current: '',
+      });
+    }
+
+    return !!match;
+  };
+
+  items = [{
+    to: Pages.OVERVIEW,
+    title: 'Обзор',
+    modifier: 'overview',
+  }, {
+    to: Pages.PAY,
+    title: 'Пополнение',
+    modifier: 'pay',
+  }, {
+    to: Pages.SERVICES,
+    title: 'Тарифы и услуги',
+    modifier: 'services',
+  }, {
+    to: Pages.ROAMING,
+    title: 'Роуминг',
+    modifier: 'roaming',
+  }, {
+    to: Pages.HISTORY,
+    title: 'История',
+    modifier: 'history',
+  }, {
+    to: Pages.SETTINGS,
+    title: 'Настройки',
+    modifier: 'settings',
+  }, {
+    to: Pages.SUPPORT_DASHBOARD,
+    title: 'Поддержка',
+    modifier: 'support-dashboard',
+  }, {
+    to: Pages.Exit,
+    title: 'Выход',
+    modifier: 'exit',
+  }];
+
+  render() {
+    const { hideLink, hideNav } = this.props;
+    const { setActiveLink, items } = this;
+    const { current } = this.state;
+    const traffic = getData('remain')[0].current.toString().replace('.', ',');
+    const calls = getData('remain')[1].current;
+
+    return (
+      <div className="aside">
+        <div className="aside__logo">
+          <img src="/media/images/logo-blue.png" alt="Next Mobile" />
+          {
+            !hideLink && <Link className="aside__home" to={Pages.OVERVIEW} />
+          }
+        </div>
         {
-          !hideLink && <Link className="aside__home" to={Pages.OVERVIEW} />
+          !hideNav &&
+          <div className="aside__inner">
+            <div className="aside__phone">+ 7 905 123-23-44</div>
+            <div className="aside__info">
+              <div>
+                <span>Баланс:</span> {formatCost(getData('balance'))}
+              </div>
+              <div>
+                <span>По России: </span>
+                <div>{traffic} ГБ, {calls} мин., БЕЗЛИМИТ СМС</div>
+              </div>
+            </div>
+            <nav className="aside__nav">
+              {
+                items.map(i => (
+                  <div key={i.modifier} className={`aside__nav-item aside__nav-item_${i.modifier}`}>
+                    <NavLink
+                      to={i.to}
+                      isActive={e => setActiveLink(e, i.modifier)}
+                      className={cs('aside__link', { 'aside__link_not-exact': current === i.modifier })}
+                    >
+                      <span>{i.title}</span>
+                    </NavLink>
+                  </div>
+                ))
+              }
+            </nav>
+          </div>
         }
       </div>
-      {
-        !hideNav &&
-        <div className="aside__inner">
-          <div className="aside__phone">+ 7 905 123-23-44</div>
-          <div className="aside__info">
-            <div>
-              <span>Баланс:</span> {formatCost(getData('balance'))}
-            </div>
-            <div>
-              <span>По России: </span>
-              <div>{traffic} ГБ, {calls} мин., БЕЗЛИМИТ СМС</div>
-            </div>
-          </div>
-          <nav className="aside__nav">
-            <div className="aside__nav-item">
-              <NavLink to={Pages.OVERVIEW} className="aside__link">Обзор</NavLink>
-            </div>
-            <div className="aside__nav-item">
-              <NavLink to={Pages.PAY} className="aside__link">Пополнение</NavLink>
-            </div>
-            <div className="aside__nav-item">
-              <NavLink to={Pages.SERVICES} className="aside__link">Тарифы и услуги</NavLink>
-            </div>
-            <div className="aside__nav-item">
-              <NavLink to={Pages.ROAMING} className="aside__link">Роуминг</NavLink>
-            </div>
-            <div className="aside__nav-item">
-              <NavLink to={Pages.HISTORY} className="aside__link">История</NavLink>
-            </div>
-            <div className="aside__nav-item">
-              <NavLink to={Pages.SETTINGS} className="aside__link">Настройки</NavLink>
-            </div>
-            <div className="aside__nav-item">
-              <NavLink to={Pages.SUPPORT_DASHBOARD} className="aside__link">Поддержка</NavLink>
-            </div>
-            <div className="aside__nav-item aside__nav-item_exit">
-              <NavLink exact to={Pages.Exit} className="aside__link">Выход</NavLink>
-            </div>
-          </nav>
-        </div>
-      }
-    </div>
-  );
-};
+    );
+  }
+}
 
 Aside.propTypes = {
   hideLink: PropTypes.bool,
