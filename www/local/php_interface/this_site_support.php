36228ce26f7549f7525a202bc1bdd7935522e3f7
Dok11 <dok@artlebedev.ru>
2018-09-25 13:01:06 +0300

    Админка. Добавил информацию о разработчике

---
+++ www/local/php_interface/this_site_support.php
@@ -0,0 +1,15 @@
+<?if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();?>
+
+<a href="//www.artlebedev.ru/">
+<table>
+	<tr>
+		<td style="padding: 0 10px 0 0;">
+			<img src="/local/tools/media/ALS_logo_admin.svg" alt="Студия Артемия Лебедева" />
+		</td>
+		<td style="text-align: left; vertical-align: top;">
+			Задизайнено <br>
+			в&nbsp;Студии Артемия Лебедева
+		</td>
+	</tr>
+</table>
+</a>
