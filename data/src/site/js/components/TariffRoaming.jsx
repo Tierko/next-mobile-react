59652d29be232af4af844848a094694c1c48c2a3
Ilya Altukhov <zompin@artlebedev.ru>
2018-10-09 18:33:45 +0300

    Typo

--- data/src/site/js/components/TariffRoaming.jsx
+++ data/src/site/js/components/TariffRoaming.jsx
@@ -5,7 +5,7 @@ import EarthTariff from './EarthTariff';
 const TariffRoaming = ({ type, size }) => (
   <div className="tariff-roaming">
     <div className="tariff-roaming__header">Роуминг</div>
-    <div>Выезжая &nbsp;командировку или на&nbsp;отдых, вы&nbsp;сразу увидите текущую стоимость связи и&nbsp;сможете легко проследить за&nbsp;расходами</div>
+    <div>Выезжая в&nbsp;командировку или на&nbsp;отдых, вы&nbsp;сразу увидите текущую стоимость связи и&nbsp;сможете легко проследить за&nbsp;расходами</div>
     <EarthTariff className="earth-tariff_tariff" tariff type={type} size={size} />
   </div>
 );
