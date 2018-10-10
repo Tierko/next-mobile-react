9efbd8e1af32a6d312495b1e8bf4749f67e092ba
Ilya Altukhov <zompin@artlebedev.ru>
2018-10-10 12:47:56 +0300

    Breadcrumbs draft

---
+++ data/src/cabinet/js/components/Breadcrumbs.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Breadcrumbs = ({ className, items }) => {
+  const el = Array(items.length).fill(null).map((_, i) => {
+    if (i % 2 === 0) {
+      const item = items[i / 2];
+
+      return (
+        <Link to={item.link} className="breadcrumbs__link">
+          {item.title}
+        </Link>
+      );
+    }
+
+    return (
+      <div className="breadcrumbs__arrow" />
+    );
+  });
+
+  if (!items.length) {
+    return false;
+  }
+
+  return (
+    <div className={`breadcrumbs ${className}`}>
+      {el}
+    </div>
+  );
+};
+
+Breadcrumbs.propTypes = {
+  className: PropTypes.string,
+  items: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+};
+
+Breadcrumbs.defaultProps = {
+  className: '',
+};
+
+export default Breadcrumbs;
