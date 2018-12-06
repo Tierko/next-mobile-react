b3c23e344dffe6f5fe902d2d733683f465551a04
Ilya Altukhov <zompin@artlebedev.ru>
2018-12-06 17:38:52 +0300

    Cards fix

--- data/src/cabinet/js/reducers/Cards.js
+++ data/src/cabinet/js/reducers/Cards.js
@@ -17,13 +17,23 @@ const initState = {
 const Cards = (state = initState, action) => {
   switch (action.type) {
   case ACTION_TYPES.CARDS_ADD:
-    return Object.assign({}, state, {items: [...state.items, action.card]});
+  {
+    let { defaultCard } = state;
+
+    if (state.items.length === 0) {
+      defaultCard = action.card.token;
+    }
+
+    return Object.assign({}, state, {
+      items: [...state.items, action.card],
+      defaultCard,
+    });
+  }
   case ACTION_TYPES.CARDS_REMOVE:
   {
     const card = state.items.find(i => i.token === action.id);
     const filteredItems = state.items.filter(i => i.token !== action.id);
     let { defaultCard } = state;
-
 
     if (card && card.token === action.id && filteredItems[0]) {
       defaultCard = filteredItems[0].token;
